ac132860d2db99bf5f01dfa017d0552c665a3469
unknown <huyhaivan@gmail.com>
2021-05-05 22:53:18 +0700

    bai4

---
+++ src/lop_va_doi_tuong_java/bai_tap/Fan.java
@@ -0,0 +1,53 @@
+package lop_va_doi_tuong_java.bai_tap;
+
+public class Fan {
+    final int SLOW = 1;
+    final int MEDIUM=2;
+    final int FAST =3;
+    private int speed = SLOW;
+    private boolean on = false;
+    private int radius = 5;
+    private String color = "Blue";
+    public Fan(){
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public int getRadius() {
+        return radius;
+    }
+    public boolean getOn(){
+        return on;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public String toString(){
+        return speed+" "+on+" "+radius+" "+color;
+    }
+    public static void main(String[] args) {
+        Fan fan1=new Fan();
+        Fan fan2=new Fan();
+        fan1.setRadius(10);
+        fan1.setColor("yellow");
+        fan1.setOn(true);
+        fan2.setSpeed(2);
+        System.out.println(fan1);
+        System.out.println(fan2);
+    }
+}
---
+++ src/lop_va_doi_tuong_java/bai_tap/StopWatch.java
@@ -0,0 +1,36 @@
+package lop_va_doi_tuong_java.bai_tap;
+
+public class StopWatch {
+    private long startTime = 0;
+    private  long endTime =0;
+    private boolean running = false;
+    public void start(){
+        this.startTime=System.currentTimeMillis();
+        this.running = true;
+    }
+    public void end(){
+        this.endTime = System.currentTimeMillis();
+        this.running = false;
+    }
+    public void setStartTime(){
+        this.startTime=System.currentTimeMillis();
+    }
+    public void setEndTime() {
+        this.endTime = System.currentTimeMillis();
+    }
+    public long getElapsedTime(){
+        if (running){
+            return (System.currentTimeMillis()-startTime);
+        }else {
+            return endTime-startTime;
+        }
+    }
+
+    public static void main(String[] args) {
+        StopWatch stopWatch= new StopWatch();
+        stopWatch.start();
+        stopWatch.end();
+        System.out.println(stopWatch.getElapsedTime());
+    }
+
+}
