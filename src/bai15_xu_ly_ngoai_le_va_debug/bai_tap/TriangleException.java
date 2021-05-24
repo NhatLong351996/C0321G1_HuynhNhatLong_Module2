6b1d98fc08ed3dcfef3780fe34b3d6dcc7c9bafc
unknown <huyhaivan@gmail.com>
2021-05-24 07:24:33 +0700

    a

--- src/bai15_xu_ly_ngoai_le_va_debug/bai_tap/TriangleException.java
+++ src/bai15_xu_ly_ngoai_le_va_debug/bai_tap/TriangleException.java
@@ -7,17 +7,18 @@ public class TriangleException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Nhập 3 canh của tam giác");
 
-            System.out.println("Cạnh thứ nhất: ");
-            double a = scanner.nextDouble();
-            System.out.println("Cạnh thứ hai: ");
-            double b = scanner.nextDouble();
-            System.out.println("Cạnh thứ ba: ");
-            double c = scanner.nextDouble();
+        System.out.println("Cạnh thứ nhất: ");
+        double a = scanner.nextDouble();
+        System.out.println("Cạnh thứ hai: ");
+        double b = scanner.nextDouble();
+        System.out.println("Cạnh thứ ba: ");
+        double c = scanner.nextDouble();
         try {
-            System.out.println("a= "+a+" b= "+b+" c= "+c);
-            IllegalTriangleException.message(a,b,c);
-        }catch (IllegalTriangleException e){
-            System.err.println(e.getMessage());
+            System.out.println("a= " + a + " b= " + b + " c= " + c);
+            IllegalTriangleException.message(a, b, c);
+        } catch (IllegalTriangleException e) {
+            /*System.err.println(e.getMessage());*/
+            throw new IllegalTriangleException();
         }
 
 
